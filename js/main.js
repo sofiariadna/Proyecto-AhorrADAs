71f3ad6ded2fed6c0db7e4356585fbd6ecff47c4
Sofia Franco <sofiaariadnafranco@gmail.com>
2023-05-19 20:38:47 -0300

    adding local storage handlers and new operation functionalities, such as saving operation data

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 //Universal Selector//
 const $=(selector)=>document.querySelector(selector)
+
 //Show or Hide Elements//
 const showElement = (selector) => $(selector).classList.remove("hidden")
 const hideElement = (selector) => $(selector).classList.add("hidden")
+
+//Random ID generator//
+const randomId = () => self.crypto.randomUUID()
+
+//Local Storage Handlers//
+const getData = (key) => JSON.parse(localStorage.getItem(key))
+const setData = (key, array) => localStorage.setItem(key, JSON.stringify(array))
+
 //Show Categories Card//
 $("#categories-btn").addEventListener("click", () => {
     hideElement("#balance-container")
@@ -45,11 +54,44 @@ $("#cancel-btn").addEventListener("click", () => {
     hideElement("#reports-card")
     hideElement("#new-operations-container")
 })
-//Add New Operation//
+//Show New Operation added//
 $("#add-btn").addEventListener("click", () => {
     hideElement("#operations-container")
     hideElement("#new-operations-container")
     showElement("#new-operation-table")
     showElement("#balance-container")
     showElement("#filters-container")
+})
+//Add New operation to the table//
+const newOperations = (operations) => {
+    for (const operation of operations) {
+        const {id, description, amount, type, category, date} = operation
+        $("#table").innerHTML += `
+        <td>${description}</td>
+        <td>${amount}</td>
+        <td>${type}</td>
+        <td>${category}</td>
+        <td>${date}</td>
+        `
+    }
+}
+//Save New operation info//
+const saveNewOperation = () => {
+    return {
+        id: randomId(),
+        description: $("#description").value,
+        amount: $("#amount").value,
+        type: $("#type").value,
+        category: $("#category").value,
+        date: $("#date").value
+    }
+}
+const newOperation = () => {
+    const currentOperation = getData("operations")
+    const newOperation = saveNewOperation()
+    currentOperation.push(newOperation)
+}
+$("#add-btn").addEventListener("click", (e) => {
+    e.preventDefault()
+    newOperation()
 })
