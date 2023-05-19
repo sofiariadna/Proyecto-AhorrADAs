2eb7520cb9b719d342f537210e39ff2933a75b70
Sofia Franco <sofiaariadnafranco@gmail.com>
2023-05-18 23:53:12 -0300

    adding new operations table. modifying show and hide elements handlers

--- js/main.js
+++ js/main.js
@@ -1,26 +1,55 @@
 //Universal Selector//
 const $=(selector)=>document.querySelector(selector)
+//Show or Hide Elements//
+const showElement = (selector) => $(selector).classList.remove("hidden")
+const hideElement = (selector) => $(selector).classList.add("hidden")
 //Show Categories Card//
 $("#categories-btn").addEventListener("click", () => {
-    $("#balance-container").style.display = "none"
-    $("#filters-container").style.display = "none"
-    $("#operations-container").style.display = "none"
-    $("#reports-card").style.display = "none"
-    $("#categories-card").style.display = "block"
+    hideElement("#balance-container")
+    hideElement("#filters-container")
+    hideElement("#operations-container")
+    hideElement("#reports-card")
+    showElement("#categories-card")
 })
 //Hide Categories Card//
 $("#balance-btn").addEventListener("click", () => {
-    $("#balance-container").style.display = "block"
-    $("#filters-container").style.display = "block"
-    $("#operations-container").style.display = "block"
-    $("#categories-card").style.display = "none"
-    $("#reports-card").style.display = "none"
+    showElement("#balance-container")
+    showElement("#filters-container")
+    showElement("#operations-container")
+    hideElement("#categories-card")
+    hideElement("#reports-card")
+    hideElement("#new-operation-table")
 })
 //Show Reports Card//
 $("#reports-btn").addEventListener("click", () => {
-    $("#balance-container").style.display = "none"
-    $("#filters-container").style.display = "none"
-    $("#operations-container").style.display = "none"
-    $("#reports-card").style.display = "block"
+    hideElement("#balance-container")
+    hideElement("#filters-container")
+    hideElement("#operations-container")
+    showElement("#reports-card")
 })
-
+//Show New Operation Card//
+$("#new-operation-btn").addEventListener("click", () => {
+    hideElement("#balance-container")
+    hideElement("#filters-container")
+    hideElement("#operations-container")
+    hideElement("#categories-card")
+    hideElement("#reports-card")
+    showElement("#new-operations-container")
+})
+//Cancel New Operation//
+$("#cancel-btn").addEventListener("click", () => {
+    showElement("#balance-container")
+    showElement("#filters-container")
+    showElement("#operations-container")
+    hideElement("#categories-card")
+    hideElement("#reports-card")
+    hideElement("#new-operations-container")
+})
+//Add New Operation//
+$("#add-btn").addEventListener("click", () => {
+    hideElement("#operations-container")
+    hideElement("#new-operations-container")
+    showElement("#new-operation-table")
+    showElement("#balance-container")
+    showElement("#filters-container")
+})
